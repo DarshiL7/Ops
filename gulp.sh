7eab79a66b743d97989b9c8990542c89b8555c97
parthshah889900 <78401922+parthshah889900@users.noreply.github.com>
2021-12-14 16:48:13 +0530

    Update gulp.sh

--- gulp.sh
+++ gulp.sh
@@ -2,4 +2,5 @@
 cd /home/php7encode/projects/devops/rxarchitecture
 npm install
 gulp
-rsync -e "ssh -p2022" -arv projects/devops/rxarchitecture/ --exclude="*.php" ansh1@3.6.79.134:~/public_html/ 
+cd
+mkdir demo15
