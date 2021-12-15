a853df5d5484debcd2c1913439072e389bb09f96
parthshah889900 <78401922+parthshah889900@users.noreply.github.com>
2021-12-15 20:01:06 +0530

    Update artifact.sh

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/php7encode/projects/devops/artifact
 rm -rf node_modules
+rm -rf .git
+rm -rf .gitignore
+rm -f ..gitignore.swp
+rm -f .htaccess
 lastServerCommit=$(date "+%d_%m_%y__%H_%M_%S")
 zip -r $lastServerCommit.zip .
 aws s3 cp ./$lastServerCommit.zip s3://opsbucketdemo/devops/
