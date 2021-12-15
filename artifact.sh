3ac6b3eca8166881b19c02ccbd233d8bc6ca72bf
parthshah889900 <78401922+parthshah889900@users.noreply.github.com>
2021-12-15 18:04:07 +0530

    Update artifact.sh

--- artifact.sh
+++ artifact.sh
@@ -3,4 +3,4 @@ cd /home/php7encode/projects/devops/artifact
 rm -rf node_modules
 lastServerCommit=$(date "+%d_%m_%y__%H_%M_%S")
 zip -r $lastServerCommit.zip .
-aws s3 cp ./artifact.zip s3://opsbucketdemo/devops/
+aws s3 cp ./$lastServerCommit.zip s3://opsbucketdemo/devops/
