add8a70166229677a0ffddbd80776398344d9a86
root <root@pc0385.radixweb.local>
2021-12-16 20:07:12 +0530

    deleted files

--- encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-project=`jq -r '.projectName.name' value.json`
-repo=`jq -r '.projectName.repoName' value.json`
-cd /home/php7encode/projects/$project/$repo
-chmod +x devopsOps.sh
-`./devopsOps.sh`
