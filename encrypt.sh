55ae3bbf4bca8c3e01fec55a4843caaf735f8b11
root <root@pc0385.radixweb.local>
2021-12-16 19:18:51 +0530

    adding

--- encrypt.sh
+++ encrypt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 project=`jq -r '.projectName.name' value.json`
-cd /home/php7encode/projects/$project/rxarchitecture
+repo=`jq -r '.projectName.repoName' value.json`
+cd /home/php7encode/projects/$project/$repo
 chmod +x devopsOps.sh
 `./devopsOps.sh`
