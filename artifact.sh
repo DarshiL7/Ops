2404e1c47e44fffa065a01b19b453936e1ee9605
root <root@pc0385.radixweb.local>
2021-12-16 11:52:00 +0530

    adding

--- artifact.sh
+++ artifact.sh
@@ -5,7 +5,6 @@ rm -rf .git
 rm -rf .gitignore
 rm -f ..gitignore.swp
 rm -f .htaccess
-lastServerCommit=$(date "+%d_%m_%y__%H_%M_%S")
-zip -r $lastServerCommit.zip .
-aws s3 cp ./$lastServerCommit.zip s3://opsbucketdemo/devops/
-rm -rf $lastServerCommit.zip
+zip -r artifact.zip .
+aws s3 cp ./artifact.zip s3://opsbucketdemo/devops/buildArtifact/
+rm -rf artifact.zip
